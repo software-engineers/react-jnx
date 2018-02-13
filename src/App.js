64f4037dbad829ad2fe65bbed19414be3bb40676
Sergio Barrera <sdbeng@gmail.com>
2018-02-12 20:26:12 -0800

    Fix bug in app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h2>Hello react Berkeley</h2>
+        <p>This is a work in progress...</p>
       </div>
     );
   }
