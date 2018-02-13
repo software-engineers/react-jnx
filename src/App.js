6929c9297c8e03ca879bffb308b980521f7921a6
Sergio Barrera <sdbeng@gmail.com>
2018-02-12 17:44:56 -0800

    Capitalize word

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Hello react berkeley</h2>
+        <h2>Hello react Berkeley</h2>
       </div>
     );
   }
