c8cf28ce1a79c3924d15064977850a3db6d51d67
Sergio Barrera <sdbeng@gmail.com>
2018-02-14 17:01:45 -0800

    Add counter state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    counter: 0
+  }
   render() {
     return (
       <div className="App">
         <h2>Hello react Berkeley</h2>
         <p>This is a work in progress app...</p>
+        <h2>counter: {this.state.counter}</h2>
       </div>
     );
   }
